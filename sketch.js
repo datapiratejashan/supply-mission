f3c68adc14425eab301362d587d8dd54625c8988
datapiratejashan <77879313+datapiratejashan@users.noreply.github.com>
2021-04-03 14:34:50 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -85,13 +85,22 @@ function draw() {
   
   drawSprites();
    
-  if(keyCode === LEFT_ARROW){
-	helicopterSprite.x=helicopterSprite.x-20;
 }
-
-if(keyCode ===  DOWN_ARROW){
-	Matter.body.setStatic(packageBody,fale)
-}
+function keyPressed() {
+	if (keyCode === LEFT_ARROW) {
   
- 
-}
+	  helicopterSprite.x=helicopterSprite.x-20;    
+	  translation={x:-20,y:0}
+	  Matter.Body.translate(packageBody, translation)
+  
+  
+	} else if (keyCode === RIGHT_ARROW) {
+	  helicopterSprite.x=helicopterSprite.x+20;
+	  translation={x:20,y:0}
+	  Matter.Body.translate(packageBody, translation)
+	}
+	else if (keyCode === DOWN_ARROW) {
+	  Matter.Body.setStatic(packageBody,false);
+	  
+	}
+  }
